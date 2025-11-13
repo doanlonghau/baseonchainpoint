1add8e763d4aba6b8332fca5aca83858ce66facc
doanlonghau <doanlonghau@gmail.com>
2025-11-13 11:30:51 +0700

    Initial Base Onchain Point miniapp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a7b3c-e26c-3027-1431-2187ecab6536',
+        permanent: false
+      }
+    ]
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
